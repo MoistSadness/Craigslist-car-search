583d075cf7e27d2c2e985bf30c05ef3829a16457
E-Waste <moistjesus@admins-MacBook-Pro.local>
2021-12-18 15:08:22 -0800

    Working

--- Car_Searcher.py
+++ Car_Searcher.py
@@ -8,16 +8,54 @@ import requests
 from bs4 import BeautifulSoup 
 
 '''
+Function takes a dictionary as input and returns a list of keys
+'''
+def getList(dict):
+    return[*dict]
+
+'''
 Function does all the searching and parsing for search results
+Runs once for every car
 '''
 def Search_For_Car(model, mileage, price):
-    print(model, mileage, price)
+    # Creating URLs, one for each location
+    URL = {}
+    URL["Bay Area"] = "https://sfbay.craigslist.org/d/cars-trucks/search/cta?query=" + model
+    #URL["Sacramento"] = "https://sacramento.craigslist.org/d/cars-trucks/search/cta?query=" + model
+
+    # getting a list of keys from the urls
+    keyListLocations = getList(URL)
+
+    # Make the vehicle search in every location in the list of locations 
+    for location in range(len(keyListLocations)):
+        # Get the webpage 
+        page = requests.get(URL[keyListLocations[location]])
+        #print(page.text)
+        #print("Page for ", URL[keyListLocations[location]])
+
+        # Creating a beautiful soup object
+        soup = BeautifulSoup(page.content, "html.parser")
+        #print(soup.prettify())
+
+        # Use Beautiful Soup to find all search results             
+        search_results = soup.find(id="search-results")
+        print(search_results.prettify())
+
+        # Use beautiful soup to find all elements by class
+        search_results_class = search_results.find_all("div", class_="result-row")
+        for result in search_results_class:
+            #print(result, end="\n*2")
+            
+
+
+
 
 
 '''
 Imports data from JSON file and uses it to run the desired searches
 '''
 def main():
+    # Open the text file containing car data
     with open("cars.json", "r") as carJSON:
         carList = json.loads(carJSON.read())
         # print(cars.read())
---
+++ README.md
@@ -0,0 +1,13 @@
+Application searches craigslist for used cars and scrapes the returned html file to search for all the related searches that fit the desired search criteria
+
+Create a bash script that runs the application? Is this even needed?
+
+Import data from the cars.json file
+Make the data passed to the function usable
+Generates all the required information (like URLs)
+Acquires the webpage from craigslist and scrapes all the URLs for the search results from it
+    Goes into each of those URLs and scrapes the page for vehicle information (Mileage, price)
+Compares the information retrieved to the desired information
+    If it is a match, it is added to a list of URLs
+    In nto a match, it is ignored
+List of URLs with a summary is emailed to the user
